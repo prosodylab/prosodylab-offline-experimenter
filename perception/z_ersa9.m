36bc7229403390d4bd69af6b9237c443980ca4c2
yasemin <yasemin.boluk@gmail.com>
2013-06-21 11:04:01 -0400

    comments added to ersa9, changed folder name for production
    comments added to perception/z_ersa9.m are Erin's from ersaperc.m

--- perception/z_ersa9.m
+++ perception/z_ersa9.m
@@ -2,21 +2,37 @@
 
 %Purpose: Main method for experiment
 % chael@mcgill.ca 11/17/09
+% comments by Erin Olson erin.daphne.olson@gmail.com 11/19/12
 
+% To stop the script at any time, press "command" + "enter"
+% If you stop the script during the experiment, you will also have to
+% blindly type "clear screen" and hit "enter"
+
+% To get information on any command in this script, type "help " followed
+% by the command name
+
+% Closes all windows and clears command history
 close all
 clear all
 
+% Adds the folder "prosodylabscripts" to the path
 addpath('prosodylabscripts');
 
 %
 % ----- Configuration --------------
 %
-% number of experiments:
 
+% This part of the script sets the number of experiment files
+% (spreadsheets) that the script needs to run overall
+
+% number of experiments:
 nExperiments = 1;
 experiments=1:nExperiments;
 [~, nexp]=size(experiments);
 
+% This part of the script sets the number of practice experiments
+% (spreadsheets) that the script needs to run - set to 0 because this part
+% does not work yet
 practice=0;
 
 % Design
@@ -42,19 +58,20 @@ designs={'Fixed' 'Random' 'PseudoRandom' 'LatinSquare' 'Blocked'};
 %     There will be as many playlists (=groups of participants)
 %     as there are conditions
 % 5 : Blocked: 
-%     Each participant sees only one conditions.
+%     Each participant see sonly one conditions.
 %     number of items has to be divisible by number of conditions
 %     There will be as many playlists (=groups of participants)
 %     as there are conditions
+design(1)=4;
 
-
-design(1)=2;
-
-% Number of questions a participant is maximially asked
-nQuestions(1)=2;
+% Number of questions a participant is maximially asked per experiment
+% Must be equivalent to or higher than the number of question columns in
+% your experiment file (spreadsheet)
+nQuestions(1)=1;
 
 
 % Is a file recorded?
+% yes = 1; no = 0
 recordedFile(nExperiments)=0;
 recordFile(1)=0;
 
@@ -62,10 +79,11 @@ recordFile(1)=0;
 % these should be a tab-separated files
 % only columns that are labeled in the header row will be
 % read into a data structure
-
-%item_file{1}='scram.txt';
+% Example: item_file{1}='scram.txt';
 item_file{1}='ersa9.txt';
 
+% This section tells the script what other folders are necessary for
+% running the script
 % relevant paths (should be given with final '/'
 path_items='1_experiment/';
 path_instructions='1_experiment/';
@@ -73,8 +91,8 @@ path_stimuli='2_stimuli/';
 path_results='2_data/';
 path_soundfiles='2_data/1_soundfiles/';
 
-%Load Experiment Settings
-% are these currently used for anything?
+% Load Experiment Settings
+% This loads the settings.m file
 [settings]=z_settings();
 
 % For dialog-experiments, specify the role of the participant
@@ -82,35 +100,40 @@ path_soundfiles='2_data/1_soundfiles/';
 % role=0;
 
 %
-%  -------------------------
+%  --------- End Configuration ----------------
 %
 
 % clear command window
 clc
 
 % Read in Experiment Files and Set Up Result File
+% For each experiment....
 for i=1:nExperiments
     
+    % Read in the experiment file
     [items{i},columnNames{i}]=tdfimport([path_items item_file{i}]);
     
-    % Set up responses File and Generate Playlist
+    % Read in the response file
     responsesFilename{i}=[path_results items{i}(1).experiment '_responses.txt'];
     
+    % If there is no response file...
     if ~exist(responsesFilename{i})
         
+        % Add the additional column names as saved in settings.m
         additionalNames=settings.additionalColNames;
         
-        % add columnname for recorded file
+        % If there is a file recorded, add a column for its name
         if recordFile(i)
             additionalNames=[ additionalNames,'recordedFile' ];
         end
         
-        % add columname for contextfile onset and offset
+        % If there is a sound file to be played, add the columns for onset
+        % and offset
         if isfield(items{i},'sound1')
-            additionalNames=[additionalNames,'firstPlayed', 'stimulusOnset_1', 'stimulusOffset_1', 'stimulusOnset_2', 'stimulusOffset_2'];
+            additionalNames=[additionalNames, 'firstPlayed', 'stimulusOnset_1', 'stimulusOffset_1', 'stimulusOnset_2', 'stimulusOffset_2'];
         end
         
-        % add columnnames for participant responses
+        % If there are columns for questions, add column names for participant responses to questions
         if isfield(items{i},'question')
             additionalNames=[additionalNames ,'response', 'keypress'];
             if isfield(items{i},'question2')
@@ -121,63 +144,85 @@ for i=1:nExperiments
             end
             
         end
-        additionalNames=sprintf('%s\t',additionalNames{:});
         
+        % Print the additional column names
+        additionalNames=sprintf('%s\t',additionalNames{:});
         columnNames{i}=sprintf('%s\t',columnNames{i}{:});
         columnNames{i}=[columnNames{i} additionalNames];
         
-        %write column headers to new response file
-        fid = fopen(responsesFilename{i},'a','l', 'UTF-8');  %open file and appending
+        % Write column headers to new response file
+        % Open file
+        fid = fopen(responsesFilename{i},'a','l', 'UTF-8');
+        % Add column names to end of file
         fprintf(fid,'%s\n',columnNames{i});
-        fclose(fid);  %close file
+        % Close the file
+        fclose(fid);
     end
 end
 
 % Get Participant number
 % check whether particpant has already taken part in experiment 1
-% if not, determine plist number
+% if not, detemrine plist number
 
 ok=0;
-while ~ok
+while ~ok %while ok=0 (which it is initially)
+    % Ask for input of participant number
     participant = input('Please enter the participant number: ', 's');
+    % Change type of data for participant number from "string" to number
+    % and check if ok (if ok, ok=1)
     [participant ok]=str2num(participant);
     if ok
+        % Run the checkSetup.m script - generates a playlist (pList) and
+        % checks if still ok
+        % If not ok, it will display the message "There is already a
+        % participant with that number!"
         [pList ok]=checkSetup(participant,design,responsesFilename,nExperiments);
     end
 end
 
-%Generate randomized list for participant
+% Generate randomized list for participant by using generatePlaylist.m
+% Gives back the playlist (playList) - which is a spreadsheet or data frame - and the number of Trials
 [playList, nTrials]=generatePlaylist(items,pList,design,experiments);
 
 for i=1:nexp
+    % Display the playList number
     disp(['experiment: ' playList{i}(1).experiment ]);
+    % Display the design type
     disp(['design: ' designs{design(i)}]);
-    disp(['length: ' num2str(nTrials)]);    
+    % Display the number of Trials
+    disp(['length: ' num2str(nTrials)]);   
+    % Display the item order
     disp(['item order: ' num2str([playList{i}(:).item ]) ]);
+    % Display the condition order
     disp(['condition order: ' num2str([ playList{i}(:).condition ] )]);
 end
 
+% Display "ok?" and wait for key press
 while KbCheck(-1); end;
 disp('ok?');
 while ~KbCheck(-1); end;
 
+% Get the keyCode from KbCheck.m (a Psychtoolbox script)
+% Note: the ~ in the script below suppresses an output so that only keyCode
+% is stored (for more information on keyCode, see the tutorial, second day)
 [~, ~, keyCode]=KbCheck(-1);
 
+% If the key pressed is n, this stops the script and asks to change the
+% design number (see line 38)
 if strcmp('n',KbName(keyCode))
     plistchoice=input('Ok! Please change the design number in the script!', 's');
 else
 
-
-
-%Display Screen
+% Displays Psychtoolbox welcome screen
 ws = doScreen(settings);
 %ListenChar(2);
 
+% Displays instructions file
 displayInstructions(ws, [ path_instructions 'instructions.txt'],settings);
 
 %
 %
-% Practice Session
+% Practice Session - Not working - skip to line 347
 %
 %
 
@@ -304,34 +349,38 @@ counter(1:nexp)=0;
 trialN=0;
 
 % Main loop for the experiment
-while max(nTrials-counter)~=0
+while max(nTrials-counter)~=0 % while the number of remaining trials is not 0...
     
-    % this should be 1:nExperiment for other experiments
+    % For 1 to number of experiments...
     for i=1:nexp
         
+        % Defines current experiment
         exp=experiments(i);
         
-        % the condition after & shouldn't be there after other experiments
-        if counter(i)<nTrials(i)
+        if counter(i)<nTrials(i) % If the counter is less than the number of trials...
             
+            % If the trial number is not 0, display message 3 as defined in
+            % settings.m. Otherwise, display message 2.
             if trialN ~= 0
                 askReady(ws,double(settings.message3),settings);
             else
                  askReady(ws,double(settings.message2),settings);
             end
             
+            % Add 1 to the trial number and counter
             trialN=trialN+1;
-            
             counter(i)=counter(i)+1;
             k=counter(i);
             
-            % add information about participant
+            % Adds information about participant to file
+            % Must be in the same order as the spreadsheet headers defined
+            % starting at line 123
             playList{i}(k).participant=participant;
             playList{i}(k).playlist=pList(exp);
             playList{i}(k).order=counter(i);
             playList{i}(k).nTrial=trialN;
             
-            %1-Display sentence to user
+            % Store contents of text column(s) to be displayed later
             if isfield(playList{i}(k),'text')
                text= [ playList{i}(k).text];
                if isfield(playList{i}(k),'text2')
@@ -340,29 +389,30 @@ while max(nTrials-counter)~=0
                         text= [text '\n\n' playList{i}(k).text3 ];
                    end
                end
-            else
+            else % If no text column, no text is stored
                 text=[];
             end
             
             context=[];
             
+            % Store contents of setup column to be displayed later
             if isfield(playList{i}(k),'setup')
                 context=playList{i}(k).setup;
-            else
+            else % If no setup column, no context is stored
                 context=[];
             end
             
+            % Store contents of context column to be displayed later
             if isfield(playList{i}(k),'context')
                 context=[ context '\n\n [' playList{i}(k).context ']'];
-            else
+            else % If no context column, no context is stored
                 context=[];
             end
             
-            % Check whether a soundfile should be played
-            
+            % Store contents of sound column to be played later
             if isfield(items{i},'sound1')
-                sound1=[ path_stimuli playList{i}(k).sound1];
-            else
+                sound1=[ path_stimuli playList{i}(k).sound];
+            else % If no sound column, no sound is stored
                 sound1=[];
             end
             
@@ -378,17 +428,20 @@ while max(nTrials-counter)~=0
                 sound2=[];
             end
             
+            % Store contents of labtext column to be displayed later
             if isfield(playList{i}(k),'labtext')
                 labtext=playList{i}(k).labtext;
-            else
+            else % If no labtext column, the labtext column is the same as the text column
                 labtext=text;
             end
                  
-            %---Display Text
+            %---Display Text - not currently in use.
+            % If not in use, it means that the participant will not see
+            % anything different while the sound is playing
             
            % while KbCheck([-1]); end;
             
-            %DrawFormattedText(ws.ptr, double(settings.message),50,300,0,settings.textwidth,[],[],1.2);
+           % DrawFormattedText(ws.ptr, double(settings.message),50,300,0,settings.textwidth,[],[],1.2);
             
            % DrawFormattedText(ws.ptr, double(context),50,400,0,settings.textwidth,[],[],1.2);
             
@@ -397,36 +450,52 @@ while max(nTrials-counter)~=0
             
             %Screen('Flip',ws.ptr);
             
-            while ~KbCheck([-1]); end;
+            while ~KbCheck([-1]); end; %Waits for key press
             
+            % If there is a recorded file (as defined on line 75), run
+            % RecordSound.m, which returns a matrix with the recorded audio
+            % data.
             if recordFile(i)==1
                 [recordedaudio]=RecordSound(contextFile,context,text,1,ws,settings);
                 
-                %Construct wave file name
+                %Construct wave file name from the values in the columns
+                %"experiment", "participant", "item", "condition" and
+                %appends the .wav extension
                 wavfilename=[playList{i}(k).experiment '_'...
                     num2str(playList{i}(k).participant) '_' ...
                     num2str(playList{i}(k).item) '_' ...
                     num2str(playList{i}(k).condition) '.wav'];
                 
+                % Stores the .wav file name to the playList spreadsheet
                 playList{i}(k).recordedFile=wavfilename;
                 
+                % Directs the script where to save the .wav file
                 wavfilename=[path_soundfiles wavfilename];
                 
+                % Writes the recorded audio data to a .wav file
                 wavwrite(transpose(recordedaudio), settings.sampfreq, 16, wavfilename);
                               
                 %Save .lab file
-                
-                %Construct .lab file name
+                %Construct .lab file name from the values in the columns
+                %"experiment", "participant", "item", "condition", and
+                %appends the .lab extension
                 labfilename=[path_soundfiles,playList{i}(k).experiment...
                     '_' num2str(playList{i}(k).participant)...
                     '_' num2str(playList{i}(k).item)...
                     '_' num2str(playList{i}(k).condition) '.lab'];
                 
-                fid = fopen([labfilename],'a','l', 'UTF-8');  %open file and appending
-                fprintf(fid,'%s\n',labtext);  %print item text to file
-                fclose(fid);  %close file
-               
-            elseif ~isempty(sound1) && ~isempty(sound2)
+                % Open file
+                fid = fopen([labfilename],'a','l', 'UTF-8');
+                % Print the stored value "labtext" to the file (defined on
+                % line 431) 
+                fprintf(fid,'%s\n',labtext);
+                % Close file
+                fclose(fid);
+            
+            % Otherwise, if the "sound" column is not empty (see line 424),
+            % play the sound back to the participant
+            elseif ~isempty(sound1) && ~isempy(sound2)
+                
                 % randomize
                 rand_index=randperm(2);
                 soundfiles={[playList{i}(k).sound1] [playList{i}(k).sound2]};
@@ -465,9 +534,17 @@ while max(nTrials-counter)~=0
             
     end
            
+           % Stores the entire playList spreadsheet to the variable
+           % "response"
            response=playList{i}(k);
             
-            % Ask Questions if there are any
+            % ----- Ask Questions -------------------------
+           
+            % Ask Questions (up to 3) if there are any columns for questions in the
+            % spreadsheet "response" by using the script askquestion.m
+            
+            % askquestion.m returns the entire spreadsheet plus the columns
+            % for the question data
             
             if isfield(playList{i}(k),'question')
                 response=askquestion(ws, settings, response,1);
@@ -476,7 +553,6 @@ while max(nTrials-counter)~=0
             end
             
             if isfield(playList{i}(k),'question2')
-                while KbCheck ; end;
                 response=askquestion(ws, settings, response,2);
             end
             
@@ -486,27 +562,32 @@ while max(nTrials-counter)~=0
             
             
             
-            %---Save Responses in Response File
+            %---Save Responses in Response File------------------
             
-            resultline=struct2cell(response);  % convert structure into cells
+            % convert matrix to cell structure
+            resultline=struct2cell(response);  
             [nColumns a]=size(resultline);
-            
             output='';
             
+            % For each column, print the resulting column to the cell
+            % structure
             for l=1:nColumns
                 output=[output sprintf('%s\t',num2str(resultline{l}))];
             end
             
-            %save response
-            fid = fopen(responsesFilename{exp},'a','l', 'UTF-8');  %open file and appending
+            % Save the response file
+            % Open file as a UTF8 file
+            fid = fopen(responsesFilename{exp},'a','l', 'UTF-8');
+            % Print cell structure
             fprintf(fid,'%s\n',output);
-            fclose(fid);  %close file
+            % Close file
+            fclose(fid);
             
         end
 end
 
 
-%Display Thank You Screen
+% Display Thank You Screen
 Screen('TextSize',ws.ptr,60);
 drawText('Thank You!',ws,1);
 
